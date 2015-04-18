fe27cc692040f766e5e72d9351d507d81af2f1fc
Zachary Brown <zachary.g.brown@gmail.com>
2015-04-17 21:53:20 -0700

    Add comments

--- run-wp-cron-multi.sh
+++ run-wp-cron-multi.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Change this variable to be the path to your WordPress installation
 wp_path='/var/www/html/thelastcicada.com'
 
 cd ${wp_path}
 
 wp site list --field=url --allow-root | while read site
 do
-        curl -L -s ${site}wp-cron.php
-        sleep 8
+        curl -L -s ${site}wp-cron.php  # -s for silent, -L to follow redirect
+        sleep 8  #Prevent all sites from running wp-cron at once
 done
